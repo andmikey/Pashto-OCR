f45c8f847b20589c943c1cd0fc3bc28943c70fa5
Mikey <31766607+andmikey@users.noreply.github.com>
2024-10-12 09:00:45 +0200

    Fix font generation paths

--- code/synthetic_data_generators.py
+++ code/synthetic_data_generators.py
@@ -5,6 +5,10 @@ from trdg.generators import GeneratorFromDict
 
 # Experiment definitions
 
+FONT_PREFIX = Path(
+    "/scratch/gusandmich/conda_envs/final_assignment_conda/lib/python3.8/site-packages/trdg/fonts/ps/"
+)
+
 baseline_experiment = GeneratorFromDict(
     language="ps",
     count=30_000,  # Generate 30k training samples
@@ -47,7 +51,7 @@ just_one_font = GeneratorFromDict(
     distorsion_type=3,
     background_type=0,
     size=64,
-    fonts=["Scheherazade-Regular.ttf"],  # Chosen arbitrarily
+    fonts=[str(FONT_PREFIX / "Scheherazade-Regular.ttf")],  # Chosen arbitrarily
 )
 
 just_five_fonts = GeneratorFromDict(
@@ -61,11 +65,11 @@ just_five_fonts = GeneratorFromDict(
     size=64,
     fonts=[
         # Chosen arbitrarily to get a mix of styles
-        "Scheherazade-Regular.ttf",
-        "MarkaziText-Bold.ttf",
-        "Bahij Helvetica Neue-Roman.ttf",
-        "Bahij Muna-Black.ttf",
-        "Bahij TheSansArabic-Light.ttf",
+        str(FONT_PREFIX / "Scheherazade-Regular.ttf"),
+        str(FONT_PREFIX / "MarkaziText-Bold.ttf"),
+        str(FONT_PREFIX / "Bahij Helvetica Neue-Roman.ttf"),
+        str(FONT_PREFIX / "Bahij Muna-Black.ttf"),
+        str(FONT_PREFIX / "Bahij TheSansArabic-Light.ttf"),
     ],
 )
 
@@ -115,21 +119,28 @@ plain_white_background = GeneratorFromDict(
 
 
 EXPERIMENTS = {
-    "baseline": baseline_experiment,
-    "baseline_150k": baseline_150k_samples,
-    "baseline_450k": baseline_450k_samples,
+    # "baseline": baseline_experiment,
+    # "baseline_150k": baseline_150k_samples,
+    # "baseline_450k": baseline_450k_samples,
     "just_one_font": just_one_font,
     "just_five_fonts": just_five_fonts,
-    "remove_skew": remove_skew,
-    "remove_distorsion": remove_distorsion,
-    "remove_blur": remove_blur,
-    "plain_white_background": plain_white_background,
+    # "remove_skew": remove_skew,
+    # "remove_distorsion": remove_distorsion,
+    # "remove_blur": remove_blur,
+    # "plain_white_background": plain_white_background,
 }
 
+# 30k images = 300MB approx
+# So 2.1GB for the 30k images
+# And 1.5GB for 150k, 5GB for 450k
+# So just under 10GB in total
+# Better to put on the SSD...
+# DATA_PATH = Path("/srv/data/gusandmich/synthetic_data")
 DATA_PATH = Path("/scratch/gusandmich/final_assignment/synthetic_data")
 
 
 def generate(exp):
+    print(f"Starting {exp}")
     write_path = DATA_PATH / exp
     write_path.mkdir(exist_ok=True)
 
@@ -153,8 +164,11 @@ def generate(exp):
 
 if __name__ == "__main__":
 
-    thread_count = 9  # Number of experiments
-    p = Pool(thread_count)
+    for e in EXPERIMENTS:
+        generate(e)
 
-    for result in p.imap(generate, EXPERIMENTS):
-        print(f"Finished processing: {result}", flush=True)
+    # thread_count = 2  # Number of experiments
+    # p = Pool(thread_count)
+
+    # for result in p.imap(generate, EXPERIMENTS):
+    #     print(f"Finished processing: {result}", flush=True)
