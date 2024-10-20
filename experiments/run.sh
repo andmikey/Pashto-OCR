968b551c8336c9103bbf79b5abed492906965d34
Mikey <31766607+andmikey@users.noreply.github.com>
2024-10-20 10:30:25 +0200

    Update gitignore, run KPTI train

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
-python3 /home/gusandmich@GU.GU.SE/PaddleOCR/tools/train.py -c configs/baseline.yaml
-# python3 /home/gusandmich@GU.GU.SE/PaddleOCR/tools/infer_rec.py -c configs/baseline.yaml \
-#     -o Global.pretrained_model=/scratch/gusandmich/final_assignment/experiment_outputs/baseline/latest \
-#     Global.load_static_weights=false \
-#     Global.infer_img=/scratch/gusandmich/final_assignment/synthetic_data/baseline/0.png
+# python3 -m paddle.distributed.launch --gpus '2,3' /home/gusandmich@GU.GU.SE/PaddleOCR/tools/train.py -c configs/baseline.yaml
+python3 /home/gusandmich@GU.GU.SE/PaddleOCR/tools/infer_rec.py -c configs/baseline.yaml \
+    -o Global.pretrained_model=/scratch/gusandmich/final_assignment/experiment_outputs/baseline_initial_train/latest \
+    Global.load_static_weights=false \
+    Global.infer_img=/scratch/gusandmich/final_assignment/synthetic_data/baseline/0.png
+#Global.infer_img=/scratch/gusandmich/final_assignment/synthetic_data/baseline/0.png
